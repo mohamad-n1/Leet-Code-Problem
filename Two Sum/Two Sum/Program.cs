c9050eb528c4182bc1845f82a1084306963e1b4f
mohamad-n1 <157669523+mohamad-n1@users.noreply.github.com>
2025-10-28 12:05:38 +0330

    file created

---
+++ Two Sum/Two Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    internal class Program
+    {
+        static int[] TwoSum(int[] nums, int target)
+        {
+            int tedad = nums.Length - 1;
+            int counter = 0;
+            int[] export = {0,0};
+            for (int i = 0; i < nums.Length-counter; i++)
+            {
+                if (nums[i] + nums[tedad] == target)
+                {
+                    export[0] = i;
+                    export[1] = tedad;
+                    return export;
+                }
+                else if (i == tedad-1)
+                {
+                    tedad--;
+                    i = -1;
+                    counter++;
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            int[] InputArray = { 150, 24, 79, 50, 88, 345, 3 }; //your Array
+            int[] p=TwoSum(InputArray, 200); //your Target
+            Console.WriteLine("{0},{1}", p[0], p[1]);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Two Sum/Two Sum/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Two Sum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Two Sum")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b7ef4ed-0644-4dda-9ec5-c5fa0b196bec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
