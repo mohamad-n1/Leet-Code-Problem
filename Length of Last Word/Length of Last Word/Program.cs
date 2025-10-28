92681794b32932804635090bc754024c419b163a
mohamad-n1 <157669523+mohamad-n1@users.noreply.github.com>
2025-10-29 02:33:38 +0330

    file Created

---
+++ Length of Last Word/Length of Last Word/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_Last_Word
+{
+    internal class Program
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            bool flag = false;
+            int counter = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+                if (s[i] != 32)
+                {
+                    counter++;
+                    flag = true;
+                }
+                else if (s[i] == 32 && flag == true)
+                    break;
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Sentence : ");
+            string str = Console.ReadLine();
+            Console.WriteLine("The Length Of LastWord is {0}",LengthOfLastWord(str));
+            Console.ReadKey();
+        }
+
+
+    }
+}
